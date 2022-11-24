f36e5214da26940cb8edbb153afd6282ef5286a1
16bithero <renzzi.adorador@gmail.com>
2022-11-24 04:31:26 -0500

    Implemented rfc

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,31 +1,29 @@
 import axios from 'axios';
 import React, { Component } from 'react'
 import './weather.css';
+
 const API_KEY = 'ecf490a8a6d9db6fa6006bca60716e27'
 const day = new Date()
-const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 const fullDate = month[day.getUTCMonth()] + ' ' + day.getDate() + ', ' + day.getFullYear()
-const dayOne = month[day.getUTCMonth()] + ' ' + day.getDate() 
+const dayOne = month[day.getUTCMonth()] + ' ' + day.getDate()
 const dayTwo = month[day.getUTCMonth()] + ' ' + (day.getDate() + 1)
 const dayThree = month[day.getUTCMonth()] + ' ' + (day.getDate() + 2)
 const dayFour = month[day.getUTCMonth()] + ' ' + (day.getDate() + 3)
 const dayFive = month[day.getUTCMonth()] + ' ' + (day.getDate() + 4)
 
-
-
 export default class Forecast extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             weather: [],
             temp: 0,
-            search: "",
+            search: "Toronto",
             city: "",
             name: "",
             pressure: "",
             humidity: "",
             wind: "",
-            pressure: "",
             maxTemp: "",
             minTemp: "",
             tempTwo: "",
@@ -39,86 +37,90 @@ export default class Forecast extends Component {
     //     this.getWeatherData()
     // }
 
-    readCity = (e) =>{
-        this.setState({...this.state, search: e.target.value})
-        console.log(this.state.search)
+    readCity = (event) => {
+        this.setState({...this.state, search: event.target.value})
     }
 
-    getWeatherData = () => {
-        axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${this.search}&appid=${API_KEY}`)
-        .then(res =>  { 
-            console.log(res.data)
-            this.setState({...this.state, weather: res.data,
-                            temp : res.data.list[0].main.temp, 
-                            city: res.data.city.name,
-                            humidity: res.data.list[0].main.humidity,    
-                            wind: res.data.list[0].wind.speed,
-                            pressure: res.data.list[0].main.pressure,
-                            maxTemp: res.data.list[0].main.temp_max,
-                            minTemp: res.data.list[0].main.temp_min,
-                            tempTwo: res.data.list[8].main.temp,
-                            tempThree: res.data.list[16].main.temp,
-                            tempFour: res.data.list[24].main.temp,
-                            tempFive: res.data.list[32].main.temp
-                        })
-        })
+    getCity = () => {
+        this.getWeatherData(this.state.city)
+        console.log('clocked')
+    }
+
+    getWeatherData = (city) => {
+        axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=Toronto&appid=${API_KEY}`)
+            .then(res => {
+                this.setState({
+                    ...this.state,
+                    weather: res.data,
+                    temp: res.data.list[0].main.temp,
+                    city: res.data.city.name,
+                    humidity: res.data.list[0].main.humidity,
+                    wind: res.data.list[0].wind.speed,
+                    pressure: res.data.list[0].main.pressure,
+                    maxTemp: res.data.list[0].main.temp_max,
+                    minTemp: res.data.list[0].main.temp_min,
+                    tempTwo: res.data.list[8].main.temp,
+                    tempThree: res.data.list[16].main.temp,
+                    tempFour: res.data.list[24].main.temp,
+                    tempFive: res.data.list[32].main.temp
+                })
+            })
     }
 
     render() {
         return (
             <>
-              <form className="form__group field">
-                <input onChange={this.readCity} type="input" className="form__field" placeholder="Change City" name="city" id='name' required />
-                <label className="form__label">Change City</label>
-                <button onSubmit={this.getWeatherData} className="bttn">Search</button>
-            </form>
-            <br></br>
-            <div className="container">
-                <div className="weather-side">
-                    <div className="weather-gradient"></div>
-                    <div className="date-container">
-                        <h1 className="date-dayname">{this.state.city}</h1><span className="date-day">{day.toLocaleDateString('en-us', {weekday: 'long'})}</span><span className="date-day">{fullDate}</span>
-                    </div>
-                    <div className="weather-container"><i className="weather-icon" data-feather="sun"></i>
+                <form className="form__group field">
+                    <input type="text" className="form__field" placeholder="Change City" onChange={(event) => this.readCity(event)}/>
+                    <label className="form__label">Change City</label>
+                    <button onClick={this.getCity} className="bttn" type='submit'>Search</button>
+                </form>
+                <br></br>
+                <div className="container">
+                    <div className="weather-side">
+                        <div className="weather-gradient"></div>
+                        <div className="date-container">
+                            <h1 className="date-dayname">{this.state.city}</h1><span className="date-day">{day.toLocaleDateString('en-us', { weekday: 'long' })}</span><span className="date-day">{fullDate}</span>
+                        </div>
+                        <div className="weather-container"><i className="weather-icon" data-feather="sun"></i>
 
-                        <h1 className="weather-temp">{(this.state.temp - 273.15).toFixed(2)}&#8451;</h1>
-                        <h3 className="weather-desc">Sunny</h3>
-                    </div>
-                </div>
-                <div className="info-side">
-                    <div className="today-info-container">
-                        <div className="today-info">
-                            <div> <span className="title">PRESSURE</span><span className="value">{this.state.pressure} hPa</span>
-                                <div className="clear"></div>
-                            </div>
-                            <div className="precipitation"> <span className="title">HUMIDITY</span><span className="value">{this.state.humidity} %</span>
-                                <div className="clear"></div>
-                            </div>
-                            <div className="humidity"> <span className="title">WIND SPEED</span><span className="value">{this.state.wind} meter/sec</span>
-                                <div className="clear"></div>
-                            </div>
-                            <div className="wind"> <span className="title">HIGHEST TEMPERATURE</span><span className="value">{(this.state.maxTemp - 273.15).toFixed(2)}&#8451;</span>
-                                <div className="clear"></div>
-                            </div>
-                            <div className="wind"> <span className="title">LOWEST TEMPERATURE</span><span className="value">{(this.state.minTemp - 273.15).toFixed(2)}&#8451;</span>
-                                <div className="clear"></div>
-                            </div>
+                            <h1 className="weather-temp">{(this.state.temp - 273.15).toFixed(2)}&#8451;</h1>
+                            <h3 className="weather-desc">Sunny</h3>
                         </div>
                     </div>
-                    <div className="week-container">
-                        <ul className="week-list">
-                            <li><img src="http://openweathermap.org/img/wn/10d@2x.png" alt="Weather icon" /><span className="day-name">{dayOne}</span><span className="day-temp">{(this.state.temp - 273.15).toFixed(2)}&#8451;</span></li>
-                            <li><img src="http://openweathermap.org/img/wn/11d@2x.png" alt="Weather icon" /><span className="day-name">{dayTwo}</span><span className="day-temp">{(this.state.tempTwo - 273.15).toFixed(2)}&#8451;</span></li>
-                            <li><img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Weather icon" /><span className="day-name">{dayThree}</span><span className="day-temp">{(this.state.tempThree - 273.15).toFixed(2)}&#8451;</span></li>
-                            <li><img src="http://openweathermap.org/img/wn/02d@2x.png" alt="Weather icon" /><span className="day-name">{dayFour}</span><span className="day-temp">{(this.state.tempFour - 273.15).toFixed(2)}&#8451;</span></li>
-                            <li><img src="http://openweathermap.org/img/wn/03d@2x.png" alt="Weather icon" /><span className="day-name">{dayFive}</span><span className="day-temp">{(this.state.tempFive - 273.15).toFixed(2)}&#8451;</span></li>
-                            <div className="clear"></div>
-                        </ul>
+                    <div className="info-side">
+                        <div className="today-info-container">
+                            <div className="today-info">
+                                <div> <span className="title">PRESSURE</span><span className="value">{this.state.pressure} hPa</span>
+                                    <div className="clear"></div>
+                                </div>
+                                <div className="precipitation"> <span className="title">HUMIDITY</span><span className="value">{this.state.humidity} %</span>
+                                    <div className="clear"></div>
+                                </div>
+                                <div className="humidity"> <span className="title">WIND SPEED</span><span className="value">{this.state.wind} meter/sec</span>
+                                    <div className="clear"></div>
+                                </div>
+                                <div className="wind"> <span className="title">HIGHEST TEMPERATURE</span><span className="value">{(this.state.maxTemp - 273.15).toFixed(2)}&#8451;</span>
+                                    <div className="clear"></div>
+                                </div>
+                                <div className="wind"> <span className="title">LOWEST TEMPERATURE</span><span className="value">{(this.state.minTemp - 273.15).toFixed(2)}&#8451;</span>
+                                    <div className="clear"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="week-container">
+                            <ul className="week-list">
+                                <li><img src="http://openweathermap.org/img/wn/10d@2x.png" alt="Weather icon" /><span className="day-name">{dayOne}</span><span className="day-temp">{(this.state.temp - 273.15).toFixed(2)}&#8451;</span></li>
+                                <li><img src="http://openweathermap.org/img/wn/11d@2x.png" alt="Weather icon" /><span className="day-name">{dayTwo}</span><span className="day-temp">{(this.state.tempTwo - 273.15).toFixed(2)}&#8451;</span></li>
+                                <li><img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Weather icon" /><span className="day-name">{dayThree}</span><span className="day-temp">{(this.state.tempThree - 273.15).toFixed(2)}&#8451;</span></li>
+                                <li><img src="http://openweathermap.org/img/wn/02d@2x.png" alt="Weather icon" /><span className="day-name">{dayFour}</span><span className="day-temp">{(this.state.tempFour - 273.15).toFixed(2)}&#8451;</span></li>
+                                <li><img src="http://openweathermap.org/img/wn/03d@2x.png" alt="Weather icon" /><span className="day-name">{dayFive}</span><span className="day-temp">{(this.state.tempFive - 273.15).toFixed(2)}&#8451;</span></li>
+                                <div className="clear"></div>
+                            </ul>
+                        </div>
                     </div>
-                    {/* <div className="location-container"><button className="location-button"> <i data-feather="map-pin"></i><span>Change location</span></button></div> */}
                 </div>
-            </div>
-        </>
+            </>
         )
     }
 }
--- src/Weather.js
+++ src/Weather.js
@@ -7,59 +7,66 @@ import { useState } from 'react'
 export default function Weather() {
     const [city, setCity] = useState('');
     const [weather, SetWeather] = useState('');
+    const API_KEY = 'ecf490a8a6d9db6fa6006bca60716e27'
+    const day = new Date()
+    const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    const fullDate = month[day.getUTCMonth()] + ' ' + day.getDate() + ', ' + day.getFullYear()
+    const dayOne = month[day.getUTCMonth()] + ' ' + day.getDate()
+    const dayTwo = month[day.getUTCMonth()] + ' ' + (day.getDate() + 1)
+    const dayThree = month[day.getUTCMonth()] + ' ' + (day.getDate() + 2)
+    const dayFour = month[day.getUTCMonth()] + ' ' + (day.getDate() + 3)
+    const dayFive = month[day.getUTCMonth()] + ' ' + (day.getDate() + 4)
 
     return (
         <>
-            <div class="form__group field">
-                <input onChange={this.readUserID}type="input" class="form__field" placeholder="Change City" name="name" id='name' required />
-                <label for="name" class="form__label">Change City</label>
-                <button className="bttn">Search</button>
-            </div>
+            <form className="form__group field">
+                <input type="text" className="form__field" placeholder="Change City" onChange={(event) => this.readCity(event)}/>
+                <label className="form__label">Change City</label>
+                <button onClick={this.getCity} className="bttn" type='submit'>Search</button>
+            </form>
             <br></br>
             <div className="container">
                 <div className="weather-side">
                     <div className="weather-gradient"></div>
                     <div className="date-container">
-                        <h2 className="date-dayname">Tuesday</h2><span className="date-day">15 Jan 2019</span><i className="location-icon" data-feather="map-pin"></i><span className="location">Paris, FR</span>
+                        <h1 className="date-dayname">{this.state.city}</h1><span className="date-day">{day.toLocaleDateString('en-us', { weekday: 'long' })}</span><span className="date-day">{fullDate}</span>
                     </div>
                     <div className="weather-container"><i className="weather-icon" data-feather="sun"></i>
-                        <h1 className="weather-temp">29°C</h1>
+
+                        <h1 className="weather-temp">{(this.state.temp - 273.15).toFixed(2)}&#8451;</h1>
                         <h3 className="weather-desc">Sunny</h3>
                     </div>
                 </div>
                 <div className="info-side">
                     <div className="today-info-container">
                         <div className="today-info">
-                            <div> <span className="title">PREDICTABILITY</span><span className="value">0 %</span>
+                            <div> <span className="title">PRESSURE</span><span className="value">{this.state.pressure} hPa</span>
                                 <div className="clear"></div>
                             </div>
-                            <div className="precipitation"> <span className="title">HUMIDITY</span><span className="value">0 %</span>
+                            <div className="precipitation"> <span className="title">HUMIDITY</span><span className="value">{this.state.humidity} %</span>
                                 <div className="clear"></div>
                             </div>
-                            <div className="humidity"> <span className="title">WIND</span><span className="value">34 %</span>
+                            <div className="humidity"> <span className="title">WIND SPEED</span><span className="value">{this.state.wind} meter/sec</span>
                                 <div className="clear"></div>
                             </div>
-                            <div className="wind"> <span className="title">AIR PRESSURE</span><span className="value">0 km/h</span>
+                            <div className="wind"> <span className="title">HIGHEST TEMPERATURE</span><span className="value">{(this.state.maxTemp - 273.15).toFixed(2)}&#8451;</span>
                                 <div className="clear"></div>
                             </div>
-                            <div className="wind"> <span className="title">UV INDEX</span><span className="value">0 km/h</span>
+                            <div className="wind"> <span className="title">LOWEST TEMPERATURE</span><span className="value">{(this.state.minTemp - 273.15).toFixed(2)}&#8451;</span>
                                 <div className="clear"></div>
                             </div>
                         </div>
                     </div>
                     <div className="week-container">
                         <ul className="week-list">
-                            <li><img src="http://openweathermap.org/img/wn/10d@2x.png" alt="Weather icon" /><span className="day-name">Monday</span><span className="day-temp">29°C</span></li>
-                            <li><img src="http://openweathermap.org/img/wn/11d@2x.png" alt="Weather icon" /><span className="day-name">Tuesday</span><span className="day-temp">21°C</span></li>
-                            <li><img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Weather icon" /><span className="day-name">Wednesday</span><span className="day-temp">08°C</span></li>
-                            <li><img src="http://openweathermap.org/img/wn/02d@2x.png" alt="Weather icon" /><span className="day-name">Thursday</span><span className="day-temp">19°C</span></li>
-                            <li><img src="http://openweathermap.org/img/wn/03d@2x.png" alt="Weather icon" /><span className="day-name">Friday</span><span className="day-temp">19°C</span></li>
-                            <li><img src="http://openweathermap.org/img/wn/04d@2x.png" alt="Weather icon" /><span className="day-name">Saturday</span><span className="day-temp">19°C</span></li>
-                            <li><img src="http://openweathermap.org/img/wn/09d@2x.png" alt="Weather icon" /><span className="day-name">Sunday</span><span className="day-temp">19°C</span></li>
+                            <li><img src="http://openweathermap.org/img/wn/10d@2x.png" alt="Weather icon" /><span className="day-name">{dayOne}</span><span className="day-temp">{(this.state.temp - 273.15).toFixed(2)}&#8451;</span></li>
+                            <li><img src="http://openweathermap.org/img/wn/11d@2x.png" alt="Weather icon" /><span className="day-name">{dayTwo}</span><span className="day-temp">{(this.state.tempTwo - 273.15).toFixed(2)}&#8451;</span></li>
+                            <li><img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Weather icon" /><span className="day-name">{dayThree}</span><span className="day-temp">{(this.state.tempThree - 273.15).toFixed(2)}&#8451;</span></li>
+                            <li><img src="http://openweathermap.org/img/wn/02d@2x.png" alt="Weather icon" /><span className="day-name">{dayFour}</span><span className="day-temp">{(this.state.tempFour - 273.15).toFixed(2)}&#8451;</span></li>
+                            <li><img src="http://openweathermap.org/img/wn/03d@2x.png" alt="Weather icon" /><span className="day-name">{dayFive}</span><span className="day-temp">{(this.state.tempFive - 273.15).toFixed(2)}&#8451;</span></li>
                             <div className="clear"></div>
                         </ul>
                     </div>
-                    {/* <div className="location-container"><button className="location-button"> <i data-feather="map-pin"></i><span>Change location</span></button></div> */}
                 </div>
             </div>
         </>
